d6c9999046381e43935b1733218b2e491f7d14ae
annakuzmenkoit-netizen <anna.kuzmenko.it@gmail.com>
2026-04-14 22:41:57 +0300

    map

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -181,8 +181,7 @@ function HomePage() {
             <AnimatedSection>
               <div className="rounded-2xl bg-muted/50 border border-border h-80 lg:h-full flex items-center justify-center text-muted-foreground">
                 <div className="text-center">
-                  <MapPinPlaceholder />
-                  <p className="mt-2 text-sm">Карта буде тут</p>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1026.510623874494!2d33.41272308980725!3d49.06934719926235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d7531fc0f95fd9%3A0xdf92253cf19ea4ec!2sUFO%20STEAM%20HUB!5e1!3m2!1suk!2sua!4v1776195455228!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
               </div>
             </AnimatedSection>
